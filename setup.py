7dfad1360f39402156fc18815e8c5cd941f6343b
RamirezAmayaS <s.ramirez34@uniandes.edu.co>
2019-02-03 13:11:07 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'rcmh',
+    description = 'Regression by clustering using Metropolis-Hastings',
+    url = 'https://arxiv.org/abs/1811.12295',
+    author = 'Simón Ramírez Amaya',
+    author_email = 's.ramirez34@uniandes.edu.co',
+    packages = [
+        'rcmh'
+    ],
+    install_requires = [
+        'pandas'
+    ],
+    include_package_data = True,
+    package_data = {
+        'rcmh': ['data/*']
+    }
+)
