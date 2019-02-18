145c5d084a7ae46e804f019942c4ffeb5459c6b7
RamirezAmayaS <s.ramirez34@uniandes.edu.co>
2019-02-17 21:15:54 -0500

    First complete untested version

--- rcmh/chain.py
+++ rcmh/chain.py
@@ -12,14 +12,23 @@ class Chain:
     def step(self):
         if self.type == 'poisson':
             distance = numpy.random.poisson(config.poisson_lambda)
+            print(f'Se considera candidato a un distancia de {distance}')
             group_candidate = self.group.reassign(distance)
             loss_candidate = transition.get_loss(group_candidate)
+            print(f'La pérdida del candidato es {loss_candidate} \n')
             alpha = transition.get_alpha(self.loss,loss_candidate,config.temperature)
             if random.random() < alpha:
+                print(f'Se acepta candidato! \n')
                 self.group = candidate_group
                 self.loss = loss_candidate
+            else:
+                print(f'Se rechaza candidato! \n')
 
     def walk(self,steps):
+        print('Iniciando caminata... \n')
         i = 0
         while i < steps:
+            print('\n')
+            print(f'Iniciando iteración {i}.. \n')
+            print(f'La pérdida actual es {self.loss} \n')
             self.step()
--- rcmh/transition.py
+++ rcmh/transition.py
@@ -1,5 +1,6 @@
 import math
 import scipy
+import pandas as pd
 from rcmh import config
 
 def count_partitions(n,d,k):
@@ -26,4 +27,22 @@ def get_alpha(loss_actual,loss_candidate,t):
     return alpha
 
 def get_loss(group):
-    pass
+    train_sample_x, train_sample_y = get_sample(group)
+    test_sample_x, test_sample_y = get_sample(group)
+    regr = linear_model.LinearRegression()
+    regr.fit(train_sample_x,train_sample_y)
+    test_hat_y = regr.predict(test_sample_x)
+    mae = -1*mean_absolute_error(test_hat_y,test_sample_y)
+    return mae
+
+def get_features_from_diag(sample,group):
+    df = pd.DataFrame(0,index=sample.index,columns=group.groups)
+    for column in config.diag_columns:
+        df.loc[sample[column]==1,group.grouping[column]] = 1
+    return df
+
+def get_sample(group):
+    sample = config.diag_csv.sample(1*10^5))
+    sample_x = pd.concat([sample[config.feature_columns],get_features_from_diag(sample,group)],axis=1)
+    sample_y = sample[config.target_columns]
+    return sample_x,sample_y
